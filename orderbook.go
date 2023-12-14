f7e0cbaf51abb4e3b6700ae1fe0184454e43d2fc
harshR <42332761+tetrex@users.noreply.github.com>
2023-12-14 22:22:53 +0530

    feat: added sorting for , limit , order , bids

--- orderbook.go
+++ orderbook.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
 )
 
@@ -18,6 +19,12 @@ type Order struct {
 	Limit     *Limit
 	TimeStamp int64
 }
+
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].TimeStamp < o[j].TimeStamp }
 
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
@@ -48,6 +55,7 @@ func (l *Limit) DeleteOrder(o *Order) {
 	l.TotalVolume -= o.Size
 
 	//TODO: resort the whole resting orders
+	sort.Sort(l.Orders)
 }
 
 type Limit struct {
@@ -55,6 +63,19 @@ type Limit struct {
 	Orders      []*Order
 	TotalVolume float64
 }
+
+type Limits []*Limit
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swbp(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price > b.Limits[j].Price }
 
 func (l *Limit) String() string {
 	return fmt.Sprintf("[price : %.2f | volume :%.2f]", l.Price, l.TotalVolume)
