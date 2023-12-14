16aad5a852a1790ed93936d6c4a08e9753d07b4c
harshR <42332761+tetrex@users.noreply.github.com>
2023-12-14 21:18:20 +0530

    feat: added delete order

--- orderbook.go
+++ orderbook.go
@@ -20,14 +20,27 @@ func NewOrder(bid bool, size float64) *Order {
 	}
 }
 
+func (o *Order) String() string {
+	return fmt.Sprintf("[size : %.2f]", o.Size)
+}
+
 func (l *Limit) AddOrder(o *Order) {
 	o.Limit = l
 	l.Orders = append(l.Orders, o)
 	l.TotalVolume += o.Size
 }
 
-func (o *Order) String() string {
-	return fmt.Sprintf("[size : %.2f]", o.Size)
+func (l *Limit) DeleteOrder(o *Order) {
+	for i := 0; i < len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders[i] = l.Orders[len(l.Orders)-1]
+			l.Orders = l.Orders[:len(l.Orders)-1]
+		}
+	}
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	//TODO: resort the whole resting orders
 }
 
 type Limit struct {
--- orderbook_test.go
+++ orderbook_test.go
@@ -7,9 +7,15 @@ import (
 
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
-	buyOrder := NewOrder(true, 5)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 8)
+	buyOrderC := NewOrder(true, 10)
 
-	l.AddOrder(buyOrder)
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
+
+	l.DeleteOrder(buyOrderB)
 
 	fmt.Println(l)
 }
